3efef9f4a3bc809e22921286b7c22afb519ef48c
Kate <curran.kate.t@gmail.com>
2019-09-14 09:46:52 +0200

    set up to display booty calls on homepage

--- up-for-booty/src/components/booty-list.js
+++ up-for-booty/src/components/booty-list.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default class AddBooty extends Component {
-    constructor(props) {
-        super(props);
-    };
+const Booty = props => (
+  <tr>
+    <td>{props.booty.firstName}</td>
+    <td>{props.booty.lastName}</td>
+    <td>
+      <Link to={"/view/"+props.booty._id}>View details</Link>
+    </td>
+  </tr>
+)
 
-    render() {
-        return (
-            <div>Here's the booty list</div>
-        )
-    };
+export default class BootyList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {booties: []};
+  }
+
+  bootyList() {
+    return this.state.booties.map(currentBooty => {
+      return <Booty booty={currentBooty} key={currentBooty._id}/>;
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Booty List</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Name</th>
+              <th>Local time</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.bootyList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
