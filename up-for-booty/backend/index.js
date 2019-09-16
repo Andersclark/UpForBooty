18b93b4f116fd90cb33e47e0e5a6d28a98700e37
Randy <anders.clark@gmail.com>
2019-09-16 11:24:37 +0200

    Mongo cloud working. Cleaned away scripting.

--- up-for-booty/backend/index.js
+++ up-for-booty/backend/index.js
@@ -10,27 +10,14 @@ const Person = require('./mongoose-models/Person');
 const app = express();
 app.use(express.json());
 
-
-const uri = 'mongodb+srv://anders:SUDEUI3HWiQPuy62@bootycluster-iu6xq.mongodb.net/test?retryWrites=true'
+const uri = 'mongodb+srv://anders:SUDEUI3HWiQPuy62@bootycluster-iu6xq.mongodb.net/test?retryWrites=true';
 mongoose.connect(uri);
 global.db = mongoose.connection;
+
 db.on('error', () => console.log('Could not connect to DB'));
 db.once('open', () => {
-    console.log('Connected to DB')
-    
-    // Script for bulkinserting persons:
-    // Person.collection.deleteMany({});
-    console.log('Bulkinserting data...')
-    let persondata = require('./mockpersons.json');
-    persondata.forEach((person) => {
-      //Person.collection.insertOne(person);
-      Person.collection.insertMany(persondata);
-    });
-
-
+    console.log('Connected to DB');
 });
-
-
 
 app.listen(5000, () => {
   console.log(' Server is running on port: 5000');
