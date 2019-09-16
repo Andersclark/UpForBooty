20583019e4f79a8eaeedd98238507dd8454aedf1
Kate <curran.kate.t@gmail.com>
2019-09-16 12:10:31 +0200

    indiv booty page populates with info from DB

--- up-for-booty/src/components/booty-details.js
+++ up-for-booty/src/components/booty-details.js
@@ -4,15 +4,14 @@ import axios from 'axios';
 export default class BootyDetails extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { booty: [] };
   }
 
   componentDidMount() {
-    console.log('booty deets mounted!');
-
     axios.get('http://localhost:5000/booty/view/' + this.props.match.params.id)
       .then(response => {
         this.setState({ booty: response.data })
-        console.log({ booty: response.data })
       })
       .catch((error) => {
         console.log(error);
@@ -20,10 +19,23 @@ export default class BootyDetails extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h3>Booty Deets</h3>
+    const bootyDeets = this.state.booty ? (
+      <div className="bootyDeets">
+        <h4>{this.state.booty.firstName} {this.state.booty.firstName}</h4>
+        <p>{this.state.booty.phoneNo}</p>
+        <p>{this.state.booty.skypeHandle}</p>
+        <p>{this.state.booty.email}</p>
+        <p>{this.state.booty.city}</p>
+        <p>{this.state.booty.country}</p>
+        <p>{this.state.booty.timezone}</p>
       </div>
-    )
+    ) :
+      (console.log('loading page'))
+
+    return (
+      <div className="container">
+        {bootyDeets}
+      </div>
+    );
   }
 }
