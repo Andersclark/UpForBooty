e98da4909a5504d7ba42eaa78033bed8830e5f35
Elliott <dahviimakes@gmail.com>
2019-09-26 14:30:53 +0200

    Update route

--- up-for-booty/backend/routes/booty.js
+++ up-for-booty/backend/routes/booty.js
@@ -13,6 +13,12 @@ router.route('/view/:id').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/update/:id').put((req, res) => {
+  Booty.findByIdAndUpdate(req.params.id, req.body)
+  .then(booty => res.json('Updated successfully!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+})
+
 router.route('/add').post((req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
