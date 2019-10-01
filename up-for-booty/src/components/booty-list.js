7d1e36b5cbb7c9f561d12e804680fe7289d59516
Andersclark <anders.clark@gmail.com>
2019-10-01 19:45:23 +0200

    more redesign
    ..

--- up-for-booty/src/components/booty-list.js
+++ up-for-booty/src/components/booty-list.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Route } from 'react-router-dom';
 import {
-  Col, Card, CardImg, CardText, CardBody,
+  Row, Col, Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
 const Booty = props => (
-      <Col sm="6">
-        <Card className="bootycard">
+      <Col sm="6" lg="3">
+        <Card sm="6" lg="3" className="bootycard">
             <CardImg top width="100%" height="40%" src={"http://localhost:5000/img/" + props.booty.picture + ".jpg"} alt={props.booty.firstName + ' ' + props.booty.lastName + ' profile picture.'} />
             <CardBody>
               <CardTitle><h3 className="bootycardheader">{props.booty.firstName} {props.booty.lastName}</h3></CardTitle>
-              <CardSubtitle><i>{props.booty.timezone} {props.booty.time.format("HH:mm")}</i></CardSubtitle>
+              <CardSubtitle><i>Location: {props.booty.timezone}</i></CardSubtitle>
+              <CardSubtitle><i>Local time: {props.booty.time.format("HH:mm")}</i></CardSubtitle>
               <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button color="info" href={"/view/" + props.booty._id}>View details</Button>
+              <Button color="info" route={"/view/" + props.booty._id}>View details</Button>
+              
+          
             </CardBody>
           </Card> 
       </Col>
@@ -28,9 +32,9 @@ export default class BootyList extends Component {
 
   render() {
     return (
-      <div>
+      <Row>
             { this.bootyList() }
-      </div>
+      </Row>
     )
   }
 }
