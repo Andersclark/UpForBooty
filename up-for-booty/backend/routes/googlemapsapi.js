e61eae8a0f71f659bfddcbdecb15eb5451b34df8
Anders Clark <anders.clark@gmail.com>
2019-10-01 11:51:29 +0200

    cleaned up google-routes
    * Removed logs
    * Removed redundant route("/search/:city")

--- up-for-booty/backend/routes/googlemapsapi.js
+++ up-for-booty/backend/routes/googlemapsapi.js
@@ -3,21 +3,7 @@ const googleClient = require('@google/maps').createClient({
   key:'AIzaSyAXX1tIstIe91lBFIFKEMj5kSmbZsIvVyU',
   Promise: Promise
 });
- 
-router.route('/search/:city').get(async (req, res) => {
-    let query = {
-      input: req.params.city,
-      inputtype: "textquery",
-      fields: "geometry"
-    }
-    let result = await googleClient.findPlace(query).asPromise()
-      .catch(err => res.status(400).json('Error: ' + err));
-    return res.json(result);
-  });
-
-
   router.route('/time/:city').get(async (req, res) => {
-    console.log("Timezone-route triggered!")
 
     let placequery = {
       input: req.params.city,
@@ -27,7 +13,7 @@ router.route('/search/:city').get(async (req, res) => {
 
     let placeResult = await googleClient.findPlace(placequery).asPromise()
       .catch(err => res.status(400).json('Error: ' + err));
-    console.log(placeResult)
+
     let time = Date.now()/1000;
     let timequery = {
       location: placeResult.json.candidates[0].geometry.location,
@@ -35,6 +21,6 @@ router.route('/search/:city').get(async (req, res) => {
     }
     let result = await googleClient.timezone(timequery).asPromise()
       .catch(err => res.status(400).json('Error: ' + err));
-      return res.json(result)  //Client should grab timeZoneId from response
+      return res.json(result.json.timeZoneId)
   });
   module.exports = router;
