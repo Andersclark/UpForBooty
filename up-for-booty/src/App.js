bae4c2aa734391782cb235bf0bd8db1613d1f6b8
Anders Clark <anders.clark@gmail.com>
2019-10-02 19:26:59 +0200

    Cleaned up clock
    ...and added to top of app

--- up-for-booty/src/App.js
+++ up-for-booty/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import moment from 'moment-timezone';
 import Navbar from "./components/navbar";
 import HomePage from "./home-page";
 import AddBooty from "./components/add-booty";
 import BootyDetails from "./components/booty-details";
+import AnalogClock from "./components/analogclock/analogclock";
+const time = moment.tz("Europe/Stockholm");
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <div className="container">
         <Navbar />
         <br />
+        <AnalogClock time={time}></AnalogClock>
         <Route path="/" exact component={HomePage} />
         <Route path="/add" component={AddBooty} />
         <Route path="/view/:id" component={BootyDetails} />
--- up-for-booty/src/components/booty-list.js
+++ up-for-booty/src/components/booty-list.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 //import store from '../store';
 import moment from 'moment-timezone';
-import Analogclock from './analogclock/analogclock.js'
+
 
 const Booty = props => (
   <tr>
-    <td><Analogclock time={props.booty.time}></Analogclock></td>
+    
     <td>{props.booty.firstName} {props.booty.lastName}</td>
     <td>{props.booty.timezone}</td>
     <td>{props.booty.time.format("HH:mm")}</td>
