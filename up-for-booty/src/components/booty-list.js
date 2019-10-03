455e9c830f6ac6ed3c63a35612086f67c86a8337
Kate <curran.kate.t@gmail.com>
2019-10-03 09:07:48 +0200

    status icon on card

--- up-for-booty/src/components/booty-list.js
+++ up-for-booty/src/components/booty-list.js
@@ -5,32 +5,34 @@ import {
   Row, Col, Card, CardImg, CardBody,
   CardTitle, CardSubtitle,
 } from 'reactstrap';
+import Icon from './icon';
 // {"http://localhost:5000/img/" + props.booty.picture + ".jpg"}
 const Booty = props => (
-      <Col sm="6" lg="3">
-        <Link to={"/view/" + props.booty._id}>
-        <Card sm="6" lg="3" className="bootycard">
-            <CardImg top className="bootycardimg" src="https://images-na.ssl-images-amazon.com/images/I/91ijGd%2BdwZL._SY606_.jpg" alt={props.booty.firstName + ' ' + props.booty.lastName + ' profile picture.'} />
-            <CardBody>
-              <CardTitle><h3 className="bootycardheader">{props.booty.firstName} {props.booty.lastName}</h3></CardTitle>
-              <CardSubtitle><i>{props.booty.timezone}</i> <i>{props.booty.time.format("HH:mm")}</i></CardSubtitle>  
-            </CardBody>
-          </Card> 
-          </Link>
-      </Col>
+  <Col sm="6" lg="3">
+    <Link to={"/view/" + props.booty._id}>
+      <Card sm="6" lg="3" className="bootycard">
+        <CardImg top className="bootycardimg" src="https://images-na.ssl-images-amazon.com/images/I/91ijGd%2BdwZL._SY606_.jpg" alt={props.booty.firstName + ' ' + props.booty.lastName + ' profile picture.'} />
+        <CardBody>
+          <Icon status={props.booty.status} ></Icon>
+          <CardTitle><h3 className="bootycardheader">{props.booty.firstName} {props.booty.lastName}</h3></CardTitle>
+          <CardSubtitle><i>{props.booty.timezone}</i> <i>{props.booty.time.format("HH:mm")}</i></CardSubtitle>
+        </CardBody>
+      </Card>
+    </Link>
+  </Col>
 )
 
 export default class BootyList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {language: store.getLanguage()}
+    this.state = { language: store.getLanguage() }
   }
 
   bootyList() {
     return this.props.list.map(currentBooty => {
       return <Booty booty={currentBooty} key={currentBooty._id} />;
     })
-  } 
+  }
 
   componentDidMount() {
     this._isMounted = true;
@@ -46,9 +48,9 @@ export default class BootyList extends Component {
 
   render() {
     return (
-        <Row>
-            { this.bootyList() }
-        </Row>
+      <Row>
+        {this.bootyList()}
+      </Row>
     )
   }
 }
